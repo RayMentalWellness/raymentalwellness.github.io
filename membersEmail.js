ab984a92411c419e3f3ef06b4638457cb17817c7
Abigayle Peterson <abigaylerose03@gmail.com>
2018-07-25 23:12:04 -1000

    updated again

--- membersEmail.js
+++ membersEmail.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-	$("#emailStuff").focuseout(function() {
+	$("#download").focuseout(function() {
 
-		$("#emailStuff").filter(function() {
-			var userEmail = $("#emailStuff").val();
+		$("#download").filter(function() {
+			var userEmail = $("#download").val();
 			var enailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 			if(!emailReg.test(userEmail)) {
 				alert("Please enter valid email");
